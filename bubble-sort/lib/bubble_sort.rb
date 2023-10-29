0e371326e5e3e59a88cdc388fe601fe57ba34660
bagir-kaff <trexhilang@gmail.com>
2023-10-29 17:00:42 +0330

    Put the algorithm into a method

--- bubble-sort/lib/bubble_sort.rb
+++ bubble-sort/lib/bubble_sort.rb
@@ -1,16 +1,17 @@
-arr = [4,3,78,2,0,2]
-i=arr.length-1
-while i>=1 do
-  j=0
-  while j<i do
-    if arr[j]>arr[j+1]
-      temp = arr[j]
-      arr[j]=arr[j+1]
-      arr[j+1] = temp
+def bubble_sort(arr)
+  i=arr.length-1
+  while i>=1 do
+    j=0
+    while j<i do
+      if arr[j]>arr[j+1]
+        temp = arr[j]
+        arr[j]=arr[j+1]
+        arr[j+1] = temp
+      end
+      # p "#{arr} i = #{i} j = #{j}"
+      j+=1
     end
-    p "#{arr} i = #{i} j = #{j}"
-    j+=1
+    i-=1
   end
-  i-=1
+  arr
 end
-p arr
